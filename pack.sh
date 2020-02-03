7fd14a25b128066297389cad38b96733b77a729a
alex <alex@echo.sjlocal>
2020-02-02 16:55:57 -0800

    Some forgotten changes and refreshing own PRoot from Termux git repo.

--- pack.sh
+++ pack.sh
@@ -12,6 +12,6 @@ echo "For $ARCH:"
 
 set-arch $ARCH
 
-tar -czvf "$PKG_DIR/root-proot-android-$ARCH.tar.gz" -C "$INSTALL_ROOT/.." root
+tar -czvf "$PKG_DIR/proot-android-$ARCH.tar.gz" -C "$INSTALL_ROOT/.." root
 
 done
