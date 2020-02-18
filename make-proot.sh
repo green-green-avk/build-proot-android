c2dd3f16b62d433c456dc9ea079d88ed14d404e3
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2020-02-17 17:25:25 -0800

    Version 0.8.

--- make-proot.sh
+++ make-proot.sh
@@ -12,8 +12,6 @@ do
 
 set-arch $ARCH
 
-make distclean || true
-
 export CFLAGS="-I$STATIC_ROOT/include -Werror=implicit-function-declaration"
 export LDFLAGS="-L$STATIC_ROOT/lib"
 export PROOT_UNBUNDLE_LOADER='libexec/proot'
@@ -23,6 +21,10 @@ then export ANDROID_PRE5=1
 else unset ANDROID_PRE5
 fi
 
+make distclean || true
 make V=1 "PREFIX=$INSTALL_ROOT" install
+make distclean || true
+CFLAGS="$CFLAGS -DUSERLAND" make V=1 "PREFIX=$INSTALL_ROOT" proot
+cp -a ./proot "$INSTALL_ROOT/bin/proot-userland"
 
 done
