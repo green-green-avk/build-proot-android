317fbf9c6947d308be0835e8a01e80222d9a3efb
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2020-10-17 21:01:21 -0700

    Version 0.9 / build script for APK variant / new NDK

--- make-proot-for-apk.sh
+++ make-proot-for-apk.sh
@@ -14,7 +14,9 @@ set-arch $ARCH
 
 export CFLAGS="-I$STATIC_ROOT/include -Werror=implicit-function-declaration"
 export LDFLAGS="-L$STATIC_ROOT/lib"
-export PROOT_UNBUNDLE_LOADER='../libexec/proot'
+export PROOT_UNBUNDLE_LOADER='.'
+export PROOT_UNBUNDLE_LOADER_NAME='libproot-loader.so'
+export PROOT_UNBUNDLE_LOADER_NAME_32='libproot-loader32.so'
 
 if [ "$SUBARCH" == 'pre5' ]
 then export ANDROID_PRE5=1
@@ -22,24 +24,20 @@ else unset ANDROID_PRE5
 fi
 
 make distclean || true
-make V=1 "PREFIX=$INSTALL_ROOT" install
+make V=1 "PREFIX=$INSTALL_ROOT-apk" install
 make distclean || true
-CFLAGS="$CFLAGS -DUSERLAND" make V=1 "PREFIX=$INSTALL_ROOT" proot
-cp -a ./proot "$INSTALL_ROOT/bin/proot-userland"
+CFLAGS="$CFLAGS -DUSERLAND" make V=1 "PREFIX=$INSTALL_ROOT-apk" proot
+cp -a ./proot "$INSTALL_ROOT-apk/bin/proot-userland"
 
 (
-cd "$INSTALL_ROOT/bin"
+cd "$INSTALL_ROOT-apk/bin"
 for FN in *
 do
 "$STRIP" "$FN"
-done
-)
-
-(
-cd "$INSTALL_ROOT/bin/$PROOT_UNBUNDLE_LOADER"
-for FN in *
-do
-"$STRIP" "$FN"
+case "$FN" in
+lib*.so) ;;
+*) mv -f "$FN" "lib$FN.so" ;;
+esac
 done
 )
 
