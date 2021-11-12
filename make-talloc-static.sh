a7fbcca571ce19cf3fbea98398631bb403f37e04
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2021-11-11 21:31:55 -0800

    A workaround for broken talloc cross-compile option
    As it turned out, it was badly broken all the time before and my
    latest local environment change has revealed it.

--- make-talloc-static.sh
+++ make-talloc-static.sh
@@ -31,36 +31,32 @@ Checking uname machine type: "dontcare"
 Checking uname release type: "dontcare"
 Checking uname version type: "dontcare"
 Checking simple C program: OK
-building library support: OK
-Checking for large file support: OK
-Checking for -D_FILE_OFFSET_BITS=64: $FILE_OFFSET_BITS
-Checking for WORDS_BIGENDIAN: OK
-Checking for C99 vsnprintf: OK
-Checking for HAVE_SECURE_MKSTEMP: OK
 rpath library support: OK
 -Wl,--version-script support: FAIL
-Checking correct behavior of strtoll: OK
-Checking correct behavior of strptime: OK
-Checking for HAVE_IFACE_GETIFADDRS: OK
-Checking for HAVE_IFACE_IFCONF: OK
-Checking for HAVE_IFACE_IFREQ: OK
 Checking getconf LFS_CFLAGS: OK
 Checking for large file support without additional flags: OK
+Checking for -D_FILE_OFFSET_BITS=64: $FILE_OFFSET_BITS
+Checking for -D_LARGE_FILES: OK
+Checking correct behavior of strtoll: OK
 Checking for working strptime: OK
+Checking for C99 vsnprintf: OK
 Checking for HAVE_SHARED_MMAP: OK
 Checking for HAVE_MREMAP: OK
 Checking for HAVE_INCOHERENT_MMAP: OK
+Checking for HAVE_SECURE_MKSTEMP: OK
 Checking getconf large file support flags work: OK
+Checking for HAVE_IFACE_IFCONF: FAIL
 EOF
 
-./configure "--prefix=$INSTALL_ROOT" --disable-rpath --disable-python --cross-compile --cross-answers=cross-answers.txt
+# A workaround for badly broken talloc cross-compile option
+export PATH="$BASE_DIR/target-mock-bin:$PATH"
 
-make
+./configure build "--prefix=$INSTALL_ROOT" --disable-rpath --disable-python --cross-compile --cross-answers=cross-answers.txt
 
 mkdir -p "$STATIC_ROOT/include"
 mkdir -p "$STATIC_ROOT/lib"
 
-ar rcs "$STATIC_ROOT/lib/libtalloc.a" bin/default/talloc*.o
+"$AR" rcs "$STATIC_ROOT/lib/libtalloc.a" bin/default/talloc*.o
 cp -f talloc.h "$STATIC_ROOT/include"
 
 done
